d0fe0e78ad56eb00f937fc4c29e5277b6c5ea342
Tu Luong <tluong@jobadder.com>
2026-07-21 22:12:35 +0700

    update code

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,7 +18,6 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">Home</s-link>
         <s-link href="/app/logs">Product logs</s-link>
       </s-app-nav>
       <Outlet />
